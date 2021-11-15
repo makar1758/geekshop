d5777a7c29c26c3dd9b92f5be1857fc2615dc849
makar1758 <86824837+makar1758@users.noreply.github.com>
2021-11-15 14:21:26 +0300

    Lesson 4 (#4)
    * Lesson 4. Data from files
    
    * Lesson 4. Auth app and user model
    
    * Lesson 4. User login and logout
    
    * Lesson 4. User CRUD
    
    * Lesson 4. Code style fix

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Product, ProductCategory
+from .models import Contact, Product, ProductCategory
 
 admin.site.register(ProductCategory)
 admin.site.register(Product)
+admin.site.register(Contact)
 # Register your models here.
