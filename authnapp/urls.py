0b8ab020de869754769e34e69cddb4154ceb0fb9
makar1758 <86824837+makar1758@users.noreply.github.com>
2021-11-22 18:12:08 +0300

    Lesson 6 (#6)
    * Lesson 6. Basket extension
    
    * Lesson 6. Hot product
    
    * Lesson 6. Basket read and delete
    
    * Lesson 6. Product page
    
    * Lesson 6. Login fix
    
    * Lesson 6. After login
    
    * Lesson 6. AJAX
    
    * Lesson 6. Code style fix

--- authnapp/urls.py
+++ authnapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-import authnapp.views as authnapp
+import basketapp.views as basketapp
 
-app_name = "authnapp"
+from .apps import BasketappConfig
+
+app_name = BasketappConfig.name
 
 urlpatterns = [
-    path("login/", authnapp.login, name="login"),
-    path("logout/", authnapp.logout, name="logout"),
-    path("register/", authnapp.register, name="register"),
-    path("edit/", authnapp.edit, name="edit"),
+    path("", basketapp.basket, name="view"),
+    path("add/<int:pk>/", basketapp.basket_add, name="add"),
+    path("remove/<int:pk>/", basketapp.basket_remove, name="remove"),
 ]
