95f0dbf133ea4c5dd1567132e2cf1b51f7a455ca
makar1758 <MKR1758@yandex.ru>
2021-11-25 16:09:33 +0300

    lesson_8. Paginator.

--- authnapp/urls.py
+++ authnapp/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
-import basketapp.views as basketapp
+import authnapp.views as authnapp
 
-from .apps import BasketappConfig
-
-app_name = BasketappConfig.name
+app_name = "authnapp"
 
 urlpatterns = [
-    path("", basketapp.basket, name="view"),
-    path("add/<int:pk>/", basketapp.basket_add, name="add"),
-    path("remove/<int:pk>/", basketapp.basket_remove, name="remove"),
+    path("login/", authnapp.login, name="login"),
+    path("logout/", authnapp.logout, name="logout"),
+    path("register/", authnapp.register, name="register"),
+    path("edit/", authnapp.edit, name="edit"),
 ]
