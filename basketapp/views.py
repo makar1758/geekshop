b34715679708cd0d90f4c15bc355840a378d4cfa
makar1758 <86824837+makar1758@users.noreply.github.com>
2021-12-13 17:51:09 +0300

    Update views.py

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,137 +1,67 @@
-from django.db import transaction
-from django.db.models.signals import pre_delete, pre_save
-from django.dispatch import receiver
-from django.forms import inlineformset_factory
-from django.shortcuts import HttpResponseRedirect, get_object_or_404
-from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
-from django.views.generic.detail import DetailView
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
+from django.template.loader import render_to_string
+from django.urls import reverse
 
 from basketapp.models import Basket
 from mainapp.models import Product
-from ordersapp.forms import OrderItemForm
-from ordersapp.models import Order, OrderItem
 
 
-class OrderList(ListView):
-    model = Order
+@login_required
+def basket(request):
+    title = "корзина"
+    basket_items = Basket.objects.filter(user=request.user).order_by("product__category")
+    content = {"title": title, "basket_items": basket_items, "media_url": settings.MEDIA_URL}
+    return render(request, "basketapp/basket.html", content)
 
-    def get_queryset(self):
-        return Order.objects.filter(user=self.request.user)
+
+@login_required
+def basket_add(request, pk):
+    if "login" in request.META.get("HTTP_REFERER"):
+        return HttpResponseRedirect(reverse("products:product", args=[pk]))
+    product = get_object_or_404(Product, pk=pk)
+    basket = Basket.objects.filter(user=request.user, product=product).first()
+
+    if not basket:
+        basket = Basket(user=request.user, product=product)
+
+    basket.quantity += 1
+    basket.save()
+
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
-class OrderItemsCreate(CreateView):
-    model = Order
-    fields = []
-    success_url = reverse_lazy("ordersapp:orders_list")
+@login_required
+def basket_remove(request, pk):
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
-    def get_context_data(self, **kwargs):
-        data = super(OrderItemsCreate, self).get_context_data(**kwargs)
-        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
 
-        if self.request.POST:
-            formset = OrderFormSet(self.request.POST)
+@login_required
+def basket_edit(request, pk, quantity):
+    if request.is_ajax():
+        try:
+            pk = int(pk)
+            quantity = int(quantity)
+        except Exception as exp:
+            print(f"Wrong input numbers! {exp}")
+            raise exp
+        new_basket_item = Basket.objects.get(pk=int(pk))
+
+        if quantity > 0:
+            new_basket_item.quantity = quantity
+            new_basket_item.save()
         else:
-            basket_items = Basket.get_items(self.request.user)
-            if len(basket_items):
-                OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=len(basket_items))
-                formset = OrderFormSet()
-                for num, form in enumerate(formset.forms):
-                    form.initial["product"] = basket_items[num].product
-                    form.initial["quantity"] = basket_items[num].quantity
-            else:
-                formset = OrderFormSet()
+            new_basket_item.delete()
 
-        data["orderitems"] = formset
-        return data
+        basket_items = Basket.objects.filter(user=request.user).order_by("product__category")
 
-    def form_valid(self, form):
-        context = self.get_context_data()
-        orderitems = context["orderitems"]
+        content = {"basket_items": basket_items, "media_url": settings.MEDIA_URL}
 
-        with transaction.atomic():
-            form.instance.user = self.request.user
-            self.object = form.save()
-            if orderitems.is_valid():
-                orderitems.instance = self.object
-                orderitems.save()
-            # Delete items in basket after order creating only
-            Basket.objects.filter(user=self.request.user).delete()
+        result = render_to_string("basketapp/includes/inc_basket_list.html", content)
 
-        # Delete empty order
-        if self.object.get_total_cost() == 0:
-            self.object.delete()
+        return JsonResponse({"result": result})
 
-        return super(OrderItemsCreate, self).form_valid(form)
-
-
-class OrderRead(DetailView):
-    model = Order
-
-    def get_context_data(self, **kwargs):
-        context = super(OrderRead, self).get_context_data(**kwargs)
-        context["title"] = "заказ/просмотр"
-        return context
-
-
-class OrderItemsUpdate(UpdateView):
-    model = Order
-    fields = []
-    success_url = reverse_lazy("ordersapp:orders_list")
-
-    def get_context_data(self, **kwargs):
-        data = super(OrderItemsUpdate, self).get_context_data(**kwargs)
-        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
-        if self.request.POST:
-            data["orderitems"] = OrderFormSet(self.request.POST, instance=self.object)
-        else:
-            data["orderitems"] = OrderFormSet(instance=self.object)
-        return data
-
-    def form_valid(self, form):
-        context = self.get_context_data()
-        orderitems = context["orderitems"]
-
-        with transaction.atomic():
-            self.object = form.save()
-            if orderitems.is_valid():
-                orderitems.instance = self.object
-                orderitems.save()
-
-        # Delete empty order
-        if self.object.get_total_cost() == 0:
-            self.object.delete()
-
-        return super(OrderItemsUpdate, self).form_valid(form)
-
-
-class OrderDelete(DeleteView):
-    model = Order
-    success_url = reverse_lazy("ordersapp:orders_list")
-
-
-def order_forming_complete(request, pk):
-    order = get_object_or_404(Order, pk=pk)
-    order.status = Order.SENT_TO_PROCEED
-    order.save()
-
-    return HttpResponseRedirect(reverse("ordersapp:orders_list"))
-
-
-@receiver(pre_save, sender=OrderItem)
-@receiver(pre_save, sender=Basket)
-def product_quantity_update_save(instance, sender, **kwargs):
-    if instance.pk:
-        """If user change quantity in order or basket"""
-        instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
-    else:
-        """If user create order or basket"""
-        instance.product.quantity -= instance.quantity
-    instance.product.save()
-
-
-@receiver(pre_delete, sender=OrderItem)
-@receiver(pre_delete, sender=Basket)
-def product_quantity_update_delete(instance, **kwargs):
-    instance.product.quantity += instance.quantity
-    instance.product.save()
